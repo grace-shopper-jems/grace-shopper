cac91b823161e1b451d3618eab869598362ccd6a
Eileen Galindo <galindo.eileen@gmail.com>
2019-04-16 18:09:07 -0400

    Working on submitting order

--- client/store/cart.js
+++ client/store/cart.js
@@ -4,6 +4,7 @@ import axios from 'axios'
  * ACTION TYPES
  */
 const ADD_TO_CART = 'ADD_TO_CART'
+const REMOVE_ITEM = 'REMOVE_ITEM'
 
 /**
  * INITIAL STATE
@@ -13,12 +14,17 @@ const cart = []
 /**
  * ACTION CREATORS
  */
-const addToCart = product => ({type: ADD_TO_CART, product})
+export const addToCart = product => ({type: ADD_TO_CART, product})
+export const removeItem = product => ({type: REMOVE_ITEM, product})
 
 export default function(state = cart, action) {
   switch (action.type) {
     case ADD_TO_CART:
-      return [...cart, action.product]
+      console.log('CART', state)
+      console.log('....ACTION.PRODUCT', action.product)
+      return [...state, action.product]
+    case REMOVE_ITEM:
+      return state.filter(product => product.id !== action.product.id)
     default:
       return state
   }

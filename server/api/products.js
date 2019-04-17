9447044ca9676dad782d0c5dede14e2553049423
Marielle Combier-Kapel <marielle.combierkapel@gmail.com>
2019-04-17 11:54:21 -0400

    route to add to order table

--- server/api/products.js
+++ server/api/products.js
@@ -1,10 +1,23 @@
 const router = require('express').Router()
-const {Product} = require('../db/models/')
+const {Product, Order} = require('../db/models/')
 
 router.get('/', async (req, res, next) => {
   try {
     const products = await Product.findAll()
     res.status(200).json(products)
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.post('/', async (req, res, next) => {
+  try {
+    const newOrder = await Order.create({
+      userid: req.session.user.id,
+      productid: Product.id
+    })
+    console.log(req.session.user)
+    res.status(201).send(newOrder)
   } catch (error) {
     next(error)
   }

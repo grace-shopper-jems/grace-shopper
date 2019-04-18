35535674bdd18d62ef3201567f5155c0b3c08a16
Jason Levine <jasonclevine@me.com>
2019-04-18 16:18:05 -0400

    navbar changes when logged in

--- client/routes.js
+++ client/routes.js
@@ -36,7 +36,7 @@ class Routes extends Component {
         {isLoggedIn && (
           <Switch>
             {/* Routes placed here are only available after logging in */}
-            <Route path="/home" component={UserHome} />
+            <Route path="/profile" component={UserHome} />
           </Switch>
         )}
         {/* Displays our Login component as a fallback */}

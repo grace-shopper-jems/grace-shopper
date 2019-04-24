55a8138858550fb39cc7588218f5ad36eb4516c1
Jason Levine <jasonclevine@me.com>
2019-04-24 11:21:59 -0400

    style changes

--- client/components/oauth-login-form.js
+++ client/components/oauth-login-form.js
@@ -3,9 +3,7 @@ import React from 'react'
 const OauthLoginForm = props => {
   return (
     <form className="oauth" method="get" action="/auth/google">
-      <button type="submit" className="btn bg-red white p1 rounded">
-        Login with Google
-      </button>
+      <button type="submit" className="bg-red white p1 rounded" />
     </form>
   )
 }
